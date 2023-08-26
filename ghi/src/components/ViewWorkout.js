93eddde875c6e45894b1ebae7826a9a4967d6992
Wayne Basile <basilew14@gmail.com>
2023-08-25 21:47:59 -0400

    feat: finish viewworkout functionality

--- ghi/src/components/ViewWorkout.js
+++ ghi/src/components/ViewWorkout.js
@@ -3,7 +3,6 @@ import useToken from "@galvanize-inc/jwtdown-for-react";
 import ViewExercise from "./ViewExcercise";
 
 
-// ------------- view workout function -------------
 function ViewWorkout() {
   const [workout, setWorkout] = useState("");
   const [editWorkout, setEditWorkout] = useState(false);
@@ -12,9 +11,8 @@ function ViewWorkout() {
   const [workoutDescription, setWorkoutDescription] = useState("");
   const [workoutActivityName, setWorkoutActivityName] = useState("");
   const { token } = useToken();
-  // ---------------------------------------------
 
-  // ------------- get request for workout -------------
+  // ------------- START OF GET workout function -------------
   const getWorkout = async (e) => {
       // do not hardcode id
     const workoutUrl = `${process.env.REACT_APP_API_HOST}/api/workouts/3/`;
@@ -29,9 +27,9 @@ function ViewWorkout() {
   useEffect(() => {
     getWorkout();
   }, []);
-  // ---------------------------------------------
+  // ------------- END OF GET workout function -------------
 
-  // ------------- put request to workout -------------
+  // ------------- START OF PUT workout function -------------
   const handleEditWorkout = async (e) => {
     e.preventDefault();
     const data = {};
@@ -53,44 +51,78 @@ function ViewWorkout() {
     const response = await fetch(url, fetchOptions);
     if (response.ok) {
       console.log(response);
+      setEditWorkout(false);
     }
   };
-  // ---------------------------------------------
+  // ------------- END OF PUT workout function -------------
+
+  // ------------- START OF setEditWorkoutFalse function -------------
+  function setEditWorkoutFalse() {
+    setEditWorkout(false);
+    setWorkout(workout);
+    setWorkoutName(workout.name);
+    setWorkoutDuration(workout.duration);
+    setWorkoutDescription(workout.description);
+    setWorkoutActivityName(workout.activity_name);
+  }
+  // ------------- END OF setEditWorkoutFalse function -------------
 
   return (
     <>
-    {/* ------ if edit workout is true ------ */}
+
       <div className="container">
         <div className="row">
           <div className="col"></div>
-
+          {/* ------------- START OF edit workout name ------------- */}
           <div className="col-10 border border-dark rounded-5 p-4 my-5">
             <div className="input-group input-group-lg mb-3">
-              <i className="bi bi-check-circle  d-flex align-self-center fs-1 me-2"></i>
-              <i className="bi bi-x-circle  d-flex align-self-center fs-1 me-3"></i>
-              <input type="text" className="form-control text-center fs-1 rounded-5 bg-secondary bg-opacity-25" placeholder="Workout"/>
+              <i className="bi bi-check-circle opacity-0 d-flex align-self-center fs-1 me-2"></i>
+              <i className="bi bi-x-circle opacity-0 d-flex align-self-center fs-1 me-3"></i>
+              { editWorkout ?
+              <input type="text" className="form-control text-center fs-1 rounded-5 bg-secondary bg-opacity-25" placeholder={workout.name} onChange={(e) => setWorkoutName(e.target.value)} value={workoutName} id="workoutName"/>
+              :
+              <input type="text" className="form-control text-center fs-1 rounded-5 bg-white border-0" placeholder={workout.name} onChange={(e) => setWorkoutName(e.target.value)} value={workoutName} id="workoutName" disabled/>
+              }
+              { editWorkout ?
+              <>
               <i onClick={(e) => handleEditWorkout(e)} className="bi bi-check-circle text-success d-flex align-self-center fs-1 ms-3"></i>
-              <i onClick={() => setEditWorkout(false)} className="bi bi-x-circle text-danger d-flex align-self-center fs-1 ms-2"></i>
+              <i onClick={(e) => setEditWorkoutFalse(e)} className="bi bi-x-circle text-danger d-flex align-self-center fs-1 ms-2"></i>
+              </>
+              :
+              <>
+              <i className="bi bi-check-circle opacity-0 d-flex align-self-center fs-1 ms-3"></i>
+              <i onClick={() => setEditWorkout(true)} className="bi bi-pencil text-dark d-flex align-self-center fs-1 ms-2"></i>
+              </>
+              }
             </div>
-
+            {/* ------------- START OF edit workout activity name ------------- */}
             <div className="input-group input-group-md mb-3">
-              <input type="text" className="form-control text-center fs-2 rounded-5 bg-secondary bg-opacity-25" placeholder="Activity"/>
+              { editWorkout ?
+              <input type="text" className="form-control text-center fs-2 rounded-5 bg-secondary bg-opacity-25" placeholder={workout.activity_name} onChange={(e) => setWorkoutActivityName(e.target.value)} value={workoutActivityName} id="workoutActivityName" />
+              :
+              <input type="text" className="form-control text-center fs-2 rounded-5 bg-white border-0" placeholder={workout.activity_name} onChange={(e) => setWorkoutActivityName(e.target.value)} value={workoutActivityName} id="workoutActivityName" disabled />
+              }
             </div>
-
+            {/* ------------- START OF edit workout duration ------------- */}
             <div className="input-group input-group-md mb-3">
-              <input type="text" className="form-control text-center fs-2 rounded-5 bg-secondary bg-opacity-25" placeholder="Duration"/>
+              { editWorkout ?
+              <input type="text" className="form-control text-center fs-2 rounded-5 bg-secondary bg-opacity-25" placeholder={workout.duration} onChange={(e) => setWorkoutDuration(e.target.value)} value={workoutDuration} id="workoutDuration" />
+              :
+              <input type="text" className="form-control text-center fs-2 rounded-5 bg-white border-0" placeholder={workout.duration} onChange={(e) => setWorkoutDuration(e.target.value)} value={workoutDuration} id="workoutDuration" disabled />
+              }
             </div>
-
+            {/* ------------- START OF edit workout description ------------- */}
             <div className="input-group input-group-sm">
-              <textarea className="form-control text-center fs-4 rounded-5 bg-secondary bg-opacity-25" placeholder="Description"></textarea>
+              { editWorkout ?
+              <textarea type="text" className="form-control text-center fs-4 rounded-5 bg-secondary bg-opacity-25" placeholder={workout.description} onChange={(e) => setWorkoutDescription(e.target.value)} rows="3" value={workoutDescription} id="workoutDescription" />
+              :
+              <textarea type="text" className="form-control text-center fs-4 rounded-5 bg-white border-0" placeholder={workout.description} onChange={(e) => setWorkoutDescription(e.target.value)} value={workoutDescription} id="workoutDescription" rows="3" disabled />
+              }
             </div>
           </div>
-
           <div className="col"></div>
         </div>
       </div>
-
-      {/*  END OF edit workout */}
       <ViewExercise />
     </>
   );
