298d86184a7cca4b40482a0e9915435697af515b
Wayne Basile <basilew14@gmail.com>
2023-09-08 16:12:14 -0400

    ci: test deploy again x15

--- ghi/src/components/AccountForm.jsx
+++ ghi/src/components/AccountForm.jsx
@@ -66,7 +66,6 @@ function AccountForm() {
           className="row login-middle-row"
           onSubmit={(e) => handleSubmit(e)}
         >
-          {/* <div className="form-row"> */}
           <div className="column">
             <BootstrapInput
               id="firstname"
@@ -107,7 +106,6 @@ function AccountForm() {
               type="text"
             />
           </div>
-          {/* </div> */}
           <div className="account-submit">
             <input
               className="btn btn-sing-up btn-login-page"
