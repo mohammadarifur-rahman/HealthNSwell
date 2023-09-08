fbf4ce9c68eb70b18b7f020d79aace66b57b97c3
Wayne Basile <basilew14@gmail.com>
2023-09-08 17:52:37 -0400

    ci: test deployment with vite variables x5

--- ghi/src/main.jsx
+++ ghi/src/main.jsx
@@ -1,9 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "bootstrap";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap-icons/font/bootstrap-icons.css";
-import "./index.css";
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
